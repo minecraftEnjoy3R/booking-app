30dfd1a403a9bce009e4c821a04f1cd1a368e4a3
minecraftEnjoy3R <limonad123123@inbox.ru>
2024-02-28 22:15:35 +0300

    header added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <div className="md:px-20">
+        <Header/>
+        {children}
+      </div>
     </html>
   );
 }
