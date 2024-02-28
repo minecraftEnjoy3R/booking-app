30e41b7ec4f3f5a86cf27de96b912a2463d48c2c
minecraftEnjoy3R <limonad123123@inbox.ru>
2024-02-28 22:28:38 +0300

    added Hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
    <div>
-   </div>
+    {/* {Hero Section} */}
+    <Hero/>
+    </div>
   );
 }
